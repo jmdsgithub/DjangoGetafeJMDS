e6ede99561829fb1eed3a28697122acb42e9309f
jmdsgithub <josemendezds@gmail.com>
2025-03-09 15:31:10 +0100

    Dom 09-03 Casa

--- webproject/aplicacion/urls.py
+++ webproject/aplicacion/urls.py
@@ -4,6 +4,6 @@ from aplicacion import views
 #todo lo que vaya por la web 'app' entra por esas dos comillas vacías q están allí en path
 urlpatterns=[
     path('', views.index, name='index'),
-    path('viernes/', views.metodoViernes, name='viernes'),
-    path('listas/', views.metodoListas, name='listas')
+    path('listas/', views.metodoListas, name='listas'),
+    path('viernes/', views.metodoViernes, name='viernes')
 ]
