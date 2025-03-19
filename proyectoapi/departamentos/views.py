66c23ac9d5abc233f2cf436ced089946875b1729
jmdsgithub <josemendezds@gmail.com>
2025-03-19 20:01:34 +0100

    Mie 19-03 ÚLTIMO DIA!!!!!!

---
+++ proyectoapi/departamentos/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render
+from departamentos.models import ServiceDepartamentos
+
+# Create your views here.
+
+def index(request):
+    servicio=ServiceDepartamentos()
+    departamentos=servicio.getDepartamentos()
+    context={
+        "departamentos":departamentos
+    }
+    return render(request, 'pages/index.html', context)
+
+def insertarDepartamento(request):
+    if ('cajaid' in request.POST):
+        servicio=ServiceDepartamentos()
+        id=request.POST['cajaid']
+        nombre=request.POST['cajanombre']
+        localidad=request.POST['cajalocalidad']
+        servicio.insertarDepartamento(id, nombre, localidad)
+        departamentos=servicio.getDepartamentos()
+        context={
+            "departamentos":departamentos
+        }
+        return render(request, 'pages/index.html', context)
+    else:
+        return render(request, 'pages/create.html')
+    
+def eliminar(request):
+    servicio=ServiceDepartamentos()
+    id=request.GET['id']
+    servicio.delete(id)
+    departamentos=servicio.getDepartamentos()
+    context={
+        "departamentos":departamentos
+    }
+    return render(request, 'pages/index.html', context)
+
+def modificar(request):
+    if('cajaid' in request.POST):
+        servicio=ServiceDepartamentos()
+        id=request.POST['cajaid']
+        nombre=request.POST['cajanombre']
+        localidad=request.POST['cajalocalidad']
+        servicio.update(id, nombre, localidad)
+        departamentos=servicio.getDepartamentos()
+        context={
+            "departamentos":departamentos
+        }        
+        return render(request, 'pages/index.html', context)
+    
+    elif('id' in request.POST):
+        id=request.GET['id']
+        dept=servicio.findDepartamento(id)
+        context={
+            "departamentos":departamentos
+        }
+    return render(request, 'pages/index.html')
